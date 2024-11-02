40902556de4ba5d0679d8b7426d92c05846c787b
Rajdeep Bastakoti <91162286+RBCodewalker@users.noreply.github.com>
2024-11-02 17:16:04 +0100

    Update data.js

--- src/data/data.js
+++ src/data/data.js
@@ -5,40 +5,40 @@ import projectPF from '../assets/projects/project_portfolio.png'
 import projectSU from '../assets/projects/project_signup.png'
 
 export const data=[
-    {
-        id:1,
-        name:"Implemented REST API, Infinite Scrolling in NextJS(TypeScript)",
-        image:projectDH,
-        github:"https://github.com/RBCodewalker/doctors-hub",
-        live:"https://doctors-hub.vercel.app/",
-    },
-    {
-        id:2,
-        name:"A ToDo/CRUD application using HTML5/CSS3/JS",
-        image:projectTODO,
-        github:"https://github.com/RBCodewalker/Simple_to_do",
-        live:"https://rbcodewalker.github.io/Simple_to_do/ ",
-    },
-    {
-        id:3,
-        name:"A general info fullstack web-app using PHP/MySQL",
-        image:projectUCL,
-        github:"https://github.com/RBCodewalker/UCL",
-        live:"https://rbcodewalker.github.io/UCL/ ",
-    },
-    {
-        id:4,
-        name:"React JS Application for my portfolio using React/Tailwind",
-        image:projectPF,
-        github:"https://github.com/RBCodewalker/portfolio",
-        live:"https://rajdeepbastakoti.com/",
-    },
-    {
-        id:5,
-        name:"A Simple Registration Feature using Angular6/NodeJS",
-        image:projectSU,
-        github:"https://github.com/RBCodewalker/Signup",
-        live:"https://rbcodewalker.github.io/Signup/",
-    },
+    // {
+    //     id:1,
+    //     name:"Implemented REST API, Infinite Scrolling in NextJS(TypeScript)",
+    //     image:projectDH,
+    //     github:"https://github.com/RBCodewalker/doctors-hub",
+    //     live:"https://doctors-hub.vercel.app/",
+    // },
+    // {
+    //     id:2,
+    //     name:"A ToDo/CRUD application using HTML5/CSS3/JS",
+    //     image:projectTODO,
+    //     github:"https://github.com/RBCodewalker/Simple_to_do",
+    //     live:"https://rbcodewalker.github.io/Simple_to_do/ ",
+    // },
+    // {
+    //     id:3,
+    //     name:"A general info fullstack web-app using PHP/MySQL",
+    //     image:projectUCL,
+    //     github:"https://github.com/RBCodewalker/UCL",
+    //     live:"https://rbcodewalker.github.io/UCL/ ",
+    // },
+    // {
+    //     id:4,
+    //     name:"React JS Application for my portfolio using React/Tailwind",
+    //     image:projectPF,
+    //     github:"https://github.com/RBCodewalker/portfolio",
+    //     live:"https://rajdeepbastakoti.com/",
+    // },
+    // {
+    //     id:5,
+    //     name:"A Simple Registration Feature using Angular6/NodeJS",
+    //     image:projectSU,
+    //     github:"https://github.com/RBCodewalker/Signup",
+    //     live:"https://rbcodewalker.github.io/Signup/",
+    // },
 
 ]
